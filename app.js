eb3ec4a8ee75e808ee30d529b33d2f02ecd02ab9
Paloma <psecunda@elon.edu>
2019-11-18 10:02:56 -0500

    Add users route

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true  })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
+const users = require("./routes/api/users");
 
 app.get("/", (req, res) => res.send("Hello Viewer"));
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
